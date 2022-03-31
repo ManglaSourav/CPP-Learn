de9c7fd6c330d85c256cc6a0cf3a64faecba1454
Sourav Mangla <mangla.sourav96@gmail.com>
2022-03-31 00:38:22 -0700

    don't know

--- Problems/Leetcode/median_two_sorted_array_4.cpp
+++ Problems/Leetcode/median_two_sorted_array_4.cpp
@@ -1,4 +1,61 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// find the kth element int the two sorted arrays
+// let us say: A[aMid] <= B[bMid], x: mid len of a, y: mid len of b, then wen can know
 //
-// Created by Sourav Mangla on 20/02/22.
-//
+// (1) there will be at least (x + 1 + y) elements before bMid
+// (2) there will be at least (m - x - 1 + n - y) = m + n - (x + y +1) elements after aMid
+// therefore
+// if k <= x + y + 1, find the kth element in a and b, but unconsidering bMid and its suffix
+// if k > x + y + 1, find the k - (x + 1) th element in a and b, but unconsidering aMid and its prefix
+int FindKth(int A[], int aL, int aR, int B[], int bL, int bR, int k) {
+    if (aL > aR) return B[bL + k - 1];
+    if (bL > bR) return A[aL + k - 1];
+
+    int aMid = (aL + aR) / 2;
+    int bMid = (bL + bR) / 2;
+
+    if (A[aMid] <= B[bMid]) {
+        if (k <= (aMid - aL) + (bMid - bL) + 1)
+            return FindKth(A, aL, aR, B, bL, bMid - 1, k);
+        else
+            return FindKth(A, aMid + 1, aR, B, bL, bR, k - (aMid - aL) - 1);
+    } else { // A[aMid] > B[bMid]
+        if (k <= (aMid - aL) + (bMid - bL) + 1)
+            return FindKth(A, aL, aMid - 1, B, bL, bR, k);
+        else
+            return FindKth(A, aL, aR, B, bMid + 1, bR, k - (bMid - bL) - 1);
+    }
+}
+
+double findMedianSortedArrays(int A[], int m, int B[], int n) {
+    /* A[0, 1, 2, ..., n-1, n] */
+    /* A[0, 1, 2, ..., m-1, m] */
+    int k = (m + n + 1) / 2;
+    double v = (double) FindKth(A, 0, m - 1, B, 0, n - 1, k);
+
+    if ((m + n) % 2 == 0) {
+        int k2 = k + 1;
+        double v2 = (double) FindKth(A, 0, m - 1, B, 0, n - 1, k2);
+        v = (v + v2) / 2;
+    }
+
+    return v;
+}
+
+
+int main() {
+
+    int nums1[] = {1, 2};
+    int nums2[] = {3, 4};
+    int m = sizeof(nums1) / sizeof(int);
+    int n = sizeof(nums2) / sizeof(int);
+    cout << findMedianSortedArrays(nums1, m, nums2, n);
+    return 0;
+}
+
+
 
--- Problems/Others/min_positive_sum_sub_arr.cpp
+++ Problems/Others/min_positive_sum_sub_arr.cpp
@@ -1,14 +1,67 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
+#include "algorithm"
 
 using namespace std;
 
-int minPosSubArr() {}
+struct bundle {
+    int low;
+    int high;
+    int sum;
+};
+
+vector<int> prefixSum(vector<int> arr, int low, int high) {
+
+    vector<int> temp(high - low);
+    temp[0] = arr[low];
+    cout << arr[low] << endl;
+    int j = 1;
+    for (auto a: temp) {
+        cout << a << " ";
+    }
+    cout << endl;
+    for (int i = low + 1; i <= high; i++) {
+        temp[j] = arr[i] + temp[j - 1];
+        j++;
+    }
+    return temp;
+}
+
+bundle minPosSubArr(vector<int>
+                    arr,
+                    int low,
+                    int high
+) {
+    if (low == high) {
+        return bundle{
+                low, high, arr[low]
+        };
+    }
+    int mid = floor((low + high) / 2);
+    struct bundle leftBundle = minPosSubArr(arr, low, mid);
+    struct bundle rightbundle = minPosSubArr(arr, mid + 1, high);
+    vector<int> B = prefixSum(arr, low, mid);
+    vector<int> C = prefixSum(arr, mid + 1, high);
+    sort(B.begin(), B.end());
+    sort(C.begin(), C.end());
+
+
+    return bundle{
+            low, high, arr[low]
+    };
+}
 
 
 int main() {
 
+    vector<int> arr = {13, 3, -25, 20, -3, 16, -35, -1};
 
+    for (auto a: prefixSum(arr, 0, 5)) {
+        cout << a << " ";
+    }
+//    cout << prefixSum(arr, 0, 5).at(4);
+//    cout << minPosSubArr(arr, 0, arr.size()).low;
     return 0;
 }
 
