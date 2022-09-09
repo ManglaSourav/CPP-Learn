b180df280312d84893e6bb45c4913771092c6eb7
Sourav Mangla <mangla.sourav96@gmail.com>
2022-09-08 19:29:48 -0700

    fix:changes

--- STL/map.cpp
+++ STL/map.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
 
     cout << "I have done this";
-
     return 0;
 }
 
--- STL/unodered_set.cc
+++ STL/unodered_set.cc
@@ -9,7 +9,13 @@ int main() {
     string str;
     cin >> str;
     std::unordered_set<char> log;
-    str.erase(std::remove_if(str.begin(), str.end(), [&](char const c) { return !(log.insert(c).second); }), str.end());
+    str.erase(
+            std::remove_if(
+                    str.begin(),
+                    str.end(),
+                    [&](char const c) { return !(log.insert(c).second); }
+            ),
+            str.end());
     std::cout << str;
     return 0;
 }
